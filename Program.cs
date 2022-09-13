9ae94f6a46431c25d64d8ae070fa2d92a7c5ccdd
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-13 23:14:20 +0300

    Среднее арифметическое столбца массива

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Введите размеры массива");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = new Random().Next(0,10);  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+}
+ 
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write("Среднее арифметическое равно");
+    Console.Write($"{ Math.Round(sum / array.GetLength(0),1)} ");
+}
+
